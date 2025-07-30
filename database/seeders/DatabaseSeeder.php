f0e5c0068d2516ed94a90d1a4158ded9004a2aac
yeab-B <yeabsirabehailu92@gmail.com>
2025-07-30 07:20:27 +0300

    Refactor DatabaseSeeder to streamline seeding process by removing commented-out user factory code

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -5,19 +5,31 @@ namespace Database\Seeders;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Database\Seeders\UserSeeder;
+use Database\Seeders\BookingSeeder;
+use Database\Seeders\RoleSeeder;
+use Database\Seeders\PermissionSeeder;
+use Database\Seeders\CategorySeeder;
+use Database\Seeders\SkillSeeder;
 
 class DatabaseSeeder extends Seeder
 {
     /**
      * Seed the application's database.
      */
-    public function run(): void
+    public function run()
     {
-        // User::factory(10)->create();
-
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+        // Call other seeders
+        $this->call([
+            
+             RoleSeeder::class,
+            PermissionSeeder::class,
+            UserSeeder::class,
+            CategorySeeder::class,
+            SkillSeeder::class,
+           
+            
+            BookingSeeder::class,
         ]);
     }
 }
