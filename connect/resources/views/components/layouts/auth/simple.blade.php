6e5d04fb7a24b3a859e98e1d4f067bbcfbcacda9
yeab-B <yeabsirabehailu92@gmail.com>
2025-08-01 10:55:53 +0300

    Update screen reader text to reflect correct application name

--- connect/resources/views/components/layouts/auth/simple.blade.php
+++ connect/resources/views/components/layouts/auth/simple.blade.php
@@ -10,7 +10,7 @@
                     <span class="flex h-9 w-9 mb-1 items-center justify-center rounded-md">
                         <x-app-logo-icon class="size-9 fill-current text-black dark:text-white" />
                     </span>
-                    <span class="sr-only">{{ config('app.name', 'Laravel') }}</span>
+                    <span class="sr-only">{{ config('app.name', 'connect-skills') }}</span>
                 </a>
                 <div class="flex flex-col gap-6">
                     {{ $slot }}
