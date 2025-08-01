7c3b5ade7141fe50c80bccd6d2816d8ed6fa5016
yeab-B <yeabsirabehailu92@gmail.com>
2025-08-01 10:55:59 +0300

    Update screen reader text to reflect correct application name

--- connect/resources/views/components/layouts/auth/split.blade.php
+++ connect/resources/views/components/layouts/auth/split.blade.php
@@ -32,7 +32,7 @@
                             <x-app-logo-icon class="size-9 fill-current text-black dark:text-white" />
                         </span>
 
-                        <span class="sr-only">{{ config('app.name', 'Laravel') }}</span>
+                        <span class="sr-only">{{ config('app.name', 'connect-skill') }}</span>
                     </a>
                     {{ $slot }}
                 </div>
